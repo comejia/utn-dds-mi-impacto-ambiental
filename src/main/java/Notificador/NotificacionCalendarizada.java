1b4310b401524e612468302c6499d1e6c825fe0e
JoaquinMaiolo <chavomaiolo@gmail.com>
2022-06-30 21:59:48 -0300

    Calendar

---
+++ src/main/java/Notificador/NotificacionCalendarizada.java
@@ -0,0 +1,11 @@
+package Notificador;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class NotificacionCalendarizada {
+  Date horaActual = new Date(System.currentTimeMillis());
+  Calendar fecha = Calendar.getInstance();
+
+
+}
