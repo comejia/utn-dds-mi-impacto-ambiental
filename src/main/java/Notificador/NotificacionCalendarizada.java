c9f60a4950e5c8c9b655e390cb77f9d002d0e913
ChavoMaiolo <chavomaiolo@gmail.com>
2022-06-30 23:56:51 -0300

    Notificacion Calendarizada

--- src/main/java/Notificador/NotificacionCalendarizada.java
+++ src/main/java/Notificador/NotificacionCalendarizada.java
@@ -2,10 +2,28 @@ package Notificador;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Timer;
 
 public class NotificacionCalendarizada {
-  Date horaActual = new Date(System.currentTimeMillis());
-  Calendar fecha = Calendar.getInstance();
+  public void notificacion(){
+    Date horaActual = new Date(System.currentTimeMillis());
+    Calendar c = Calendar.getInstance();
+    c.setTime(horaActual);
+    if(c.get(Calendar.HOUR_OF_DAY)>10 | c.get(Calendar.DAY_OF_MONTH)!=1){
+      c.set(Calendar.DAY_OF_MONTH,1);
+      if(c.get(Calendar.MONTH)==Calendar.DECEMBER){
+        c.set(Calendar.YEAR,c.get(Calendar.YEAR)+1);
+        c.set(Calendar.MONTH, 1);
+      }else {
+        c.set(Calendar.MONTH, c.get(Calendar.MONTH) + 1);
+      }
+    }
+    c.set(Calendar.HOUR_OF_DAY, 10);
+    c.set(Calendar.MINUTE, 0);
+    c.set(Calendar.SECOND, 0);
 
-
+    Date momentoNotificacion = c.getTime();
+    Timer temporizador = new Timer();
+    temporizador.schedule(new Temporizador(), momentoNotificacion);
+  }
 }
