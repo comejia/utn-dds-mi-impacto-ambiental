ba73d7c733b7415d21d79c393f22c27bc8a5dfc3
comejia <comejia@teco.com.ar>
2022-10-06 19:04:17 -0300

    Fix run tests

--- src/test/java/db/TransporteTest.java
+++ src/test/java/db/TransporteTest.java
@@ -1,14 +1,13 @@
 package db;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 import transportes.*;
 import trayectos.Parada;
-import trayectos.Trayecto;
 
-import javax.persistence.EntityTransaction;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +18,16 @@ public class TransporteTest extends AbstractPersistenceTest implements WithGloba
   TransportePublico colectivo;
   VehiculoParticular scaloneta;
   Bicicleta bicicleta;
+
+  @BeforeEach
+  public void setup() {
+    super.setup();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    super.tearDown();
+  }
 
   @BeforeEach
   public void init(){
@@ -43,30 +52,24 @@ public class TransporteTest extends AbstractPersistenceTest implements WithGloba
 
   @Test
   public void sePuedeRecuperarTransportes() {
-    EntityTransaction tx = entityManager().getTransaction();
-    tx.begin();
     entityManager().persist(colectivo);
     entityManager().persist(scaloneta);
-    tx.commit();
+
     assertEquals(2, entityManager().createQuery("from Transporte").getResultList().size());
   }
 
   @Test
   public void sePuedeRecuperarSoloTransportesPrivados(){
-    EntityTransaction tx = entityManager().getTransaction();
-    tx.begin();
     entityManager().persist(colectivo);
     entityManager().persist(scaloneta);
-    tx.commit();
+
     assertEquals(1, entityManager().createQuery("from TransportePrivado").getResultList().size());
   }
 
   @Test
   public void sePuedeRecuperarUnTransporte(){
-    EntityTransaction tx = entityManager().getTransaction();
-    tx.begin();
     entityManager().persist(scaloneta);
-    tx.commit();
+
     Transporte scaloneta1 = entityManager().createQuery("from Transporte",Transporte.class).getResultList().get(0);
     assertEquals(scaloneta1,scaloneta);
   }
