4e76499edc74d49593efafb1e4d028009240523a
mariano <marianrais13@gmail.com>
2022-11-25 01:27:36 -0300

    html

--- src/main/java/dominio/organizaciones/Vinculacion.java
+++ src/main/java/dominio/organizaciones/Vinculacion.java
@@ -8,7 +8,7 @@ import dominio.usuarios.Usuario;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
+@Entity
 public class Vinculacion extends EntidadPersistente {
 
     @ManyToOne(cascade = CascadeType.ALL)
@@ -16,6 +16,9 @@ public class Vinculacion extends EntidadPersistente {
 
     @ManyToOne(cascade = CascadeType.ALL)
     private Usuario empleado;
+
+    public Vinculacion() {
+    }
 
     public Vinculacion(Organizacion organizacion, Usuario empleado) {
         this.organizacion = organizacion;
