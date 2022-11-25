b21b704ac13c3ef39ced85ae1562ede7941256fa
Gdepablo <guille_de_pablo@outlook.com>
2022-11-25 16:14:00 -0300

    WIP

--- src/main/java/dominio/repositorios/RepositorioOrganizacion.java
+++ src/main/java/dominio/repositorios/RepositorioOrganizacion.java
@@ -14,17 +14,15 @@ public class RepositorioOrganizacion implements WithGlobalEntityManager {
 
     public static RepositorioOrganizacion instance = new RepositorioOrganizacion();
 
-    Organizacion UTN = new Organizacion(
-            "DDS", TipoOrganizacion.INSTITUCION, new Direccion("Lugano", "Mozart", "2300"), Clasificacion.UNIVERSIDAD);
 
     public void agregar(Organizacion organizacion) {
         entityManager().persist(organizacion);
     }
 
     public List<Organizacion> listar() {
-        List<Organizacion> organizaciones = new ArrayList<>();
-        organizaciones.add(UTN);
-        return organizaciones;
+        return entityManager()
+                .createQuery("from Organizacion", Organizacion.class)
+                .getResultList();
     }
 
     public Organizacion buscarOrganizacion(int id) {
